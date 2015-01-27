875a049f2330feaeef760757470cf016a5806d4f
Phil Scarratt <fil@rubratec.com.au>
2015-01-27 23:42:46 +0000

    Changes made by Phil Scarratt (fil@rubratec.com.au).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,27 @@
 (function() {
     document.addEventListener("deviceready", function () {
+        console.log("Device Ready function");
         var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        $.get("http://illawarrasouthernhighlandsflpn.com.au/directory",function(data){
+            var doc = $(data);
+            var catList = [];
+            doc.find("#service-type").parent().find(".cn-cat-children label").each(function(){
+                catList.push($(this).text());
+            });
+            $("#directory-type").kendoMobileListView({
+                dataSource: catList,
+                template: "<span>#: data #</span>"
+            });
+        });
+        $("#directory-lga").kendoMobileListView({
+            dataSource: ["Bega","Eurobodalla","Kiama","Shellharbour","Shoalhaven","Wollondilly","Wollongong"],
+            template: "<span>#: data #</span>"
+        });
         navigator.splashscreen.hide();
     });
 }());
+
+function enterSearch(){
+    var me = $(this);
+    if(me.val()=="search")me.val("");
+}
